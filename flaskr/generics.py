b44ddf39f4dc32ee3df34e872d288a27f57628fd
Deadly <info.saddiki@gmail.com>
2022-04-28 18:58:47 +0200

    Added product endpoint blueprint

--- flaskr/generics.py
+++ flaskr/generics.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, jsonify
-from flask_jwt_extended import get_jwt_identity
+from flask import Blueprint
 
 from services import config
 
---
+++ flaskr/product.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from services.database import db_session
+from models.user import User
+from models.product import Product
+from services import config
+from services.utilities import Utilities, admin_required
+
+from datetime import datetime
+
+config = config.Config().get_config()
+auth = Blueprint('product', __name__, url_prefix='/product')
+
+
+@auth.route("/all", methods=['GET'])
+@jwt_required()
+def get_all_products():
+    """
+    TODO: Update this docstring
+    Simply checks the connection status and if the application exists.
+
+    :return: JSON-form representing aliveness?
+    """
+
+    user = User.query.filter_by(uuid=get_jwt_identity()).first()
+
+    if user is None:
+        return Utilities.return_unauthorized()
+
+    user.active = True
+    user.last_login_at = datetime.utcnow()
+    user.last_login_ip = request.remote_addr
+    user.last_action = "GET_ALL_PRODUCTS"
+    user.last_action_at = datetime.utcnow()
+
+    db_session.commit()
+
+    local_products = Product.query.all()
+
+    if local_products is None or []:
+        return Utilities.return_not_found()
+
+    result = []
+
+    for local_product in local_products:
+        result.append(local_product)
+
+    return {"result": result}, 200
