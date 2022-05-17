c34197b65e16bfc8f3b822da7ef2eeb1b44d5234
Deadly <info.saddiki@gmail.com>
2022-05-17 19:30:05 +0200

    Added event support for orders, added order endpoint

---
+++ models/event.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Float, String, PickleType, Integer, Boolean, DateTime
+
+from services.database import Base
+
+from uuid import uuid4
+from datetime import datetime, timedelta
+from dataclasses import dataclass
+
+
+@dataclass
+class Event(Base):
+    __tablename__ = 'events'
+    # Core information
+    id: int = Column(Integer, primary_key=True, nullable=False, unique=True)
+    uuid: str = Column(String(36), nullable=False, default=str(uuid4()))
+
+    # Unique tracking information
+    active: bool = Column(Boolean, nullable=False, default=True)
+    created_at: datetime = Column(DateTime, nullable=False, default=datetime.utcnow())
+    until: datetime = Column(DateTime, nullable=False, default=datetime.utcnow() + timedelta(days=7))
+    deadline: datetime = Column(DateTime, nullable=False, default=datetime.utcnow() + timedelta(days=4))
+
+    # Event unique settings
+    max_order_price: float = Column(Float, nullable=False, default=20.0)
+
+    def __repr__(self):
+        return f"<Event {self.name}>"
+
